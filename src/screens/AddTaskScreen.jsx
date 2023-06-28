d3f69a90e71d6ececfc98f677e67a9c6a7eaa85c
nina <nina.barater@alunos.sc.senac.br>
2023-06-28 15:43:19 -0300

    ta dando merda

--- src/screens/AddTaskScreen.jsx
+++ src/screens/AddTaskScreen.jsx
@@ -1,14 +1,41 @@
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 import { styles } from "../utils/styles";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
-export default function AddTaskScreen({navigation}) {
-    
+export default function AddTaskScreen({ navigation }) {
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [data, setData] = useState("");
 
+  const docRef = addDoc(collection(db, "tarefa"), {
+    titulo: titulo,
+    descricao: descricao,
+    data: new Date(),
+  });
 
-    return (
-        <View style={styles.container}>
-            <Text>Add Task Screen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+        <View style={styles.header}>
+      <Text>Cadastro</Text>
+
+      <TextInput label="Título" value={titulo} onChangeText={setTitulo} />
+      <TextInput
+        label="Descrição"
+        value={descricao}
+        onChangeText={setDescricao}
+      />
+      <TextInput label="Data de Cadastro" value={data} onChangeText={setData} />
+
+      <Button
+        labelStyle={{ fontWeight: "bold", color: "black" }}
+        onPress={() => navigation.navigate("HomeScreen")}
+      >
+        Entrar em conta existente
+      </Button>
+      </View>
+    </View>
+  );
 }
