ee66657992e81336108f972bc851116175d1baa8
Adriano Ramos <adriano.ramos3208@gmail.com>
2023-06-28 15:56:15 -0300

    add

--- src/screens/EditTaskScreen.jsx
+++ src/screens/EditTaskScreen.jsx
@@ -1,11 +1,18 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import { styles } from "../utils/styles";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+
 
 export default function EditTaskScreen({navigation}) {
     return (
-        <View style={styles.container}>
-            <Text>Edit Task Screen</Text>
-        </View>
+        <View style={styles.box}>
+                <Text style={styles.titulo}>
+                    Edit</Text>
+                </View>
+
     );
 }
