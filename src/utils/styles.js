24656e8b1368a3291497937629b10d88530f80d3
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-06-28 14:22:42 -0300

    add

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#C5E6C6',
       alignItems: 'center',
       justifyContent: 'center',
     },
