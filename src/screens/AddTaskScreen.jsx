f9130fc80ad862d7ee39f0b82c8edfedaa73bf5c
nina <nina.barater@alunos.sc.senac.br>
2023-06-28 14:54:19 -0300

    add

--- src/screens/AddTaskScreen.jsx
+++ src/screens/AddTaskScreen.jsx
@@ -3,6 +3,9 @@ import { Text } from "react-native-paper";
 import { styles } from "../utils/styles";
 
 export default function AddTaskScreen({navigation}) {
+    
+
+
     return (
         <View style={styles.container}>
             <Text>Add Task Screen</Text>
