c50b8777f5cffaa6370806ba7104d9af26e3bb03
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-06-28 15:43:44 -0300

    add

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,22 @@
 import { Image, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { styles } from "../utils/styles";
 
 export default function HomeScreen({navigation}) {
     return (
         <View style={styles.container}>
+            <Image source={require('/assets/icone.png')} style={{width: 300, height: 200,}}/>
             <View style={styles.box}>
-                <Text style={styles.text}>Home Screen</Text>
+                <Text style={styles.titulo}>MENU</Text>
+                <Button style={styles.botao} textColor={'black'} mode="contained" onPress={() => navigation.navigate('AddTaskScreen')}>
+                    Add Task
+                </Button>
+                <Button style={styles.botao} textColor={'black'} mode="contained" onPress={() => navigation.navigate('EditTaskScreen')}>
+                    Edit Task
+                </Button>
+                <Button style={styles.botao} textColor={'black'} mode="contained" onPress={() => navigation.navigate('Task')}>
+                    List Task
+                </Button>
             </View>
         </View>
     );
